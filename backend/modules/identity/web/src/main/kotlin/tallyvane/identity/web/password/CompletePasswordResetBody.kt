64337eacc7cfe373790d4a0309da983285ca3885
Yurii Surzhykov <yuriisurzhykov@gmail.com>
2026-09-25 02:10:15 -0700

    feat(auth): add serialization support to `CompletePasswordResetBody`
    - Annotate `CompletePasswordResetBody` with `@Serializable` for Kotlin serialization compatibility.
    
    Signed-off-by: Yurii Surzhykov <yuriisurzhykov@gmail.com>

--- backend/modules/identity/web/src/main/kotlin/tallyvane/identity/web/password/CompletePasswordResetBody.kt
+++ backend/modules/identity/web/src/main/kotlin/tallyvane/identity/web/password/CompletePasswordResetBody.kt
@@ -1,5 +1,8 @@
 package tallyvane.identity.web.password
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 internal data class CompletePasswordResetBody(
     val challengeId: String,
     val email: String,
